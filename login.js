6d469ca482d69e8d601b2391e74faca197972f13
JOOxCRACK <r9546232@gmail.com>
2025-07-10 23:35:51 +0300

    login.js

--- login.js
+++ login.js
@@ -89,7 +89,7 @@ app.post("/login", async (req, res) => {
 });
 
 // ✅ شغّل السيرفر
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
